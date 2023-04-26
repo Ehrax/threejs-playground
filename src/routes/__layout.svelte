dd2c3cf7187bc28515ac1d540374771098cea94b
Ehrax <alexander.rasputin@icloud.com>
2023-04-26 11:05:44 +0200

    🔨 chore(.gitignore): add .idea directory to gitignore 🔨 chore(__layout.svelte): fix indentation and change label to div in __layout.svelte file

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,65 +1,65 @@
 <script>
-  import '../app.css';
-  import Icon from '@iconify/svelte';
-  import SvelteMarkdown from 'svelte-markdown';
-  import { modal } from '../stores/info-modal-store.js';
-  import { routes } from '../utils/routes.js';
+	import '../app.css';
+	import Icon from '@iconify/svelte';
+	import SvelteMarkdown from 'svelte-markdown';
+	import { modal } from '../stores/info-modal-store.js';
+	import { routes } from '../utils/routes.js';
 
-  let menuCheckbox;
+	let menuCheckbox;
 
-  function toggleMenuDrawer() {
-    menuCheckbox.checked = !menuCheckbox.checked;
-  }
+	function toggleMenuDrawer() {
+		menuCheckbox.checked = !menuCheckbox.checked;
+	}
 </script>
 
-<input type="checkbox" class="modal-toggle" checked={!!$modal}/>
+<input type="checkbox" class="modal-toggle" checked={!!$modal} />
 <div class="modal">
-  <div class="modal-box w-11/12 max-w-3xl w-full h-full">
-    <label class="btn btn-sm btn-circle absolute right-2 top-2" on:click={() => modal.closeModal()}>
-      ✕
-    </label>
-    {#if $modal}
-      <article class="prose">
-        <SvelteMarkdown class="modal-content" source={$modal}/>
-      </article>
-    {/if}
-  </div>
+	<div class="modal-box max-w-3xl w-full h-full">
+		<div class="btn btn-sm btn-circle absolute right-2 top-2" on:click={() => modal.closeModal()}>
+			✕
+		</div>
+		{#if $modal}
+			<article class="prose">
+				<SvelteMarkdown source={$modal} />
+			</article>
+		{/if}
+	</div>
 </div>
 
 <div class="drawer">
-  <input bind:this={menuCheckbox} type="checkbox" class="drawer-toggle"/>
-  <div class="drawer-content">
-    <label class="menu-button" on:click={() => toggleMenuDrawer()}>
-      <Icon icon="ant-design:menu-outlined" />
-    </label>
+	<input bind:this={menuCheckbox} type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content">
+		<div class="menu-button" on:click={() => toggleMenuDrawer()}>
+			<Icon icon="ant-design:menu-outlined" />
+		</div>
 
-    <!-- Page content here -->
-    <slot/>
-  </div>
-  <div class="drawer-side">
-    <label class="drawer-overlay" on:click={() => toggleMenuDrawer()}/>
-    <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-      <!-- Sidebar content here -->
-      {#each routes as item}
-        <li class="mb-4">
-          <a href={item.link} class="text-base-content" on:click={() => toggleMenuDrawer()}>
-            {item.title}
-          </a>
-        </li>
-      {/each}
-    </ul>
-  </div>
+		<!-- Page content here -->
+		<slot />
+	</div>
+	<div class="drawer-side">
+		<div class="drawer-overlay" on:click={() => toggleMenuDrawer()} />
+		<ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+			<!-- Sidebar content here -->
+			{#each routes as item}
+				<li class="mb-4">
+					<a href={item.link} class="text-base-content" on:click={() => toggleMenuDrawer()}>
+						{item.title}
+					</a>
+				</li>
+			{/each}
+		</ul>
+	</div>
 </div>
 
 <style>
-    .menu-button {
-        @apply btn btn-circle btn-outline;
-        @apply absolute;
-        @apply top-4 left-4;
-    }
+	.menu-button {
+		@apply btn btn-circle btn-outline;
+		@apply absolute;
+		@apply top-4 left-4;
+	}
 
-    .modal-box {
-        max-height: calc(100% - 5rem);
-        @apply max-w-prose;
-    }
+	.modal-box {
+		max-height: calc(100% - 5rem);
+		@apply max-w-prose;
+	}
 </style>
